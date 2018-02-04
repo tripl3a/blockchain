7fcebf465a8359312bd1650f62e2c18e257c0519
tripl3a <triple_a@mailbox.org>
2018-02-04 12:43:32 +0100

    changed variable names to make them intuitively readable

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -4,24 +4,24 @@ from blockchain.blockchain import Blockchain
 def test_new_block_invalid_proof():
     bc = Blockchain()
     prev_len = len(bc.chain)
-    pb = bc.last_block
+    prev_block = bc.last_block
     # provide invalid proof number:
-    nb = bc.new_block(666)
+    new_block = bc.new_block(666)
     # was the block added to the chain?
     assert prev_len+1 == len(bc.chain)
     # was it mined correctly?
     # TODO: does it make sense to add the block when the proof isn't valid??
-    assert bc.valid_proof(pb["proof"], nb["proof"]) is False
+    assert bc.valid_proof(prev_block["proof"], new_block["proof"]) is False
 
 
 def test_new_block_valid_proof():
     bc = Blockchain()
     prev_len = len(bc.chain)
-    pb = bc.last_block
+    prev_block = bc.last_block
     # provide valid proof number:
-    nb = bc.new_block(35293)
+    new_block = bc.new_block(35293)
     # was the block added to the chain?
     assert prev_len+1 == len(bc.chain)
     # was it mined correctly?
-    assert bc.valid_proof(pb["proof"], nb["proof"]) is True
+    assert bc.valid_proof(prev_block["proof"], new_block["proof"]) is True
 
